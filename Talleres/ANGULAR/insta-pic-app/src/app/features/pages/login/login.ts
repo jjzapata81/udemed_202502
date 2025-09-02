88fed90e7d7e15719bd9363767b28d447e51a316
OnemaYepes <jumayemo108@gmail.com>
2025-09-01 20:21:49 -0500

    Se sube la práctica 2

--- Talleres/ANGULAR/insta-pic-app/src/app/features/pages/login/login.ts
+++ Talleres/ANGULAR/insta-pic-app/src/app/features/pages/login/login.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
 
+  fb = inject(FormBuilder);
+
+  ruta = '/home';
+
+  title = 'Registro de usuario';
+
+  validators = [Validators.required, Validators.minLength(4)];
+
+  loginForm = this.fb.group({
+    username:['jjzapata', [Validators.required]],
+    password:['', this.validators]
+  })
+
+
+  onLogin() {
+    if (!this.loginForm.valid) {
+      alert('Faltan campos por diligenciar');
+      return;
+    }
+
+    let user = this.loginForm.value;
+    console.log(user);
+
+    let userString =localStorage.getItem(user.username!)
+
+
+    if(userString && user.password === JSON.parse(userString)['password']){
+      alert('Bienvenido de nuevo');
+      return;
+    } else {
+      alert('Usuario no encontrado o contraseña incorrecta');
+    }
+  }
 }
