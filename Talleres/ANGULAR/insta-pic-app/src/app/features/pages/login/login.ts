4c84e334832e97d5fab2ec160edc20ce92584e94
Sebastián T. <98891697+SebastianT454@users.noreply.github.com>
2025-09-02 07:09:00 -0500

    Ejercicio Login Ts

--- Talleres/ANGULAR/insta-pic-app/src/app/features/pages/login/login.ts
+++ Talleres/ANGULAR/insta-pic-app/src/app/features/pages/login/login.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component , inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
 
+    fb = inject(FormBuilder);
+    validators = [Validators.required, Validators.minLength(4)];
+
+    signInForm = this.fb.group({
+        username:['', [Validators.required]],
+        password:['', this.validators],
+      })
+
+    onSignIn(){
+      if(!this.signInForm.valid){
+        alert('Faltan campos por diligenciar');
+        return;
+      }
+
+      let localUser = this.signInForm.value;
+      console.log(localUser);
+
+      if(localStorage.getItem(localUser.username!)){
+        const userValue: string | null = localStorage.getItem(localUser.username!)
+
+        if(userValue){
+          const userObj = JSON.parse(userValue)
+          if(userObj.password == localUser.password){
+            alert("Bienvenido al sitio");
+          }
+          else{
+            alert("Contrasena incorrecta");
+          }
+        }
+    }
+    else{
+      alert("Usuario incorrecto");
+    }
+  }
 }
