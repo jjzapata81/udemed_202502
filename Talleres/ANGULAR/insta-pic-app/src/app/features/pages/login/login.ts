e21274be90e9f9b308bd55058adb1b5d0652be13
JHONCE79 <jhonsteven.163@gmail.com>
2025-09-01 19:43:03 -0500

    agrego_login

--- Talleres/ANGULAR/insta-pic-app/src/app/features/pages/login/login.ts
+++ Talleres/ANGULAR/insta-pic-app/src/app/features/pages/login/login.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
 
+  fb = inject(FormBuilder);
+
+  title = 'Inicio de sesión';
+
+  validators = [Validators.required, Validators.minLength(4)];
+
+  loginForm = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', this.validators]
+  });
+
+  onLogin() {
+    if (!this.loginForm.valid) {
+      alert('Faltan campos por diligenciar');
+      return;
+    }
+
+    let credentials = this.loginForm.value;
+    console.log(credentials);
+
+    // Verificar si el usuario existe en localStorage
+    const userData = localStorage.getItem(credentials.username!);
+    
+    if (!userData) {
+      alert('Usuario no encontrado');
+      return;
+    }
+
+    // Parsear los datos del usuario
+    const user = JSON.parse(userData);
+
+    // Verificar la contraseña
+    if (user.password === credentials.password) {
+      alert('Inicio de sesión exitoso');
+      // Aquí podrías redirigir al usuario o establecer un estado de sesión
+    } else {
+      alert('Contraseña incorrecta');
+    }
+  }
 }
