4fdd26b35c863493eba8f8385406ecf8c668e71a
Felipe Ortiz <fortizalvaran@gmail.com>
2025-09-01 20:15:09 -0500

    Trabajo de clase 2

--- Talleres/ANGULAR/insta-pic-app/src/app/features/pages/login/login.ts
+++ Talleres/ANGULAR/insta-pic-app/src/app/features/pages/login/login.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+
+  fb = inject(FormBuilder);
+
+  LoginForm = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  })
+
+  onLogin() {
+    if (!this.LoginForm.valid) {
+      alert('Faltan campos por diligenciar');
+      return;
+    }
+    let user = this.LoginForm.value;
+    console.log(user);
+    let userStr = localStorage.getItem(user.username!)
+
+    if (userStr) {
+      let userRegistered = JSON.parse(userStr)
+      if (userRegistered.password === user.password) {
+        alert('Usuario loggeado exitosamnete');
+        return;
+      }
+    }
+    alert('Login incorrecta');
+  }
 
 }
