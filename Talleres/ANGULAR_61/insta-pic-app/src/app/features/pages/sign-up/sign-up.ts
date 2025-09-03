ed4ca875e35b014df2215bdcb7027f9dae3ba63a
Your Name <you@example.com>
2025-09-02 19:25:44 -0500

    Recative Forms

--- Talleres/ANGULAR_61/insta-pic-app/src/app/features/pages/sign-up/sign-up.ts
+++ Talleres/ANGULAR_61/insta-pic-app/src/app/features/pages/sign-up/sign-up.ts
@@ -1,12 +1,40 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 import { RouterLink } from "@angular/router";
 
 @Component({
     selector:'app-sign-up',
-    imports:[RouterLink],
+    imports:[RouterLink, ReactiveFormsModule],
     templateUrl:'./sign-up.html',
     styleUrl:'./sign-up.css'
 })
 export class SignUp{
+
+    title = 'Registro de usuario';
+
+    fb = inject(FormBuilder);
+
+    signUpForm = this.fb.group({
+        username:['', [Validators.required]],
+        email:['', [Validators.required]],
+        password:['', [Validators.required, Validators.minLength(4)]],
+        rePassword:['', [Validators.required, Validators.minLength(4)]]
+    })
+
+
+    onSignUp(){
+        if(!this.signUpForm.valid){
+            alert('Formulario no es valido');
+            return;
+        }
+        let user = this.signUpForm.value
+        console.log(user)
+
+        let userStr = JSON.stringify(user)
+
+        localStorage.setItem(user.username!, userStr);    
+
+
+    }
 
 }
