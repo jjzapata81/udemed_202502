0cd79bd23a8572ed7854e2519377878fb5a4bdce
jjzapata <jjzapata@bancolombia.com.co>
2025-09-08 15:00:37 -0500

    home refactor

--- Talleres/ANGULAR/insta-pic-app/src/app/features/pages/home/home.ts
+++ Talleres/ANGULAR/insta-pic-app/src/app/features/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +7,64 @@ import { Component } from '@angular/core';
   styleUrl: './home.css'
 })
 export class Home {
+
+  posts = 25;
+  followers = 48;
+  requests = 37;
+
+  galleryItems = signal([
+    {
+      id: 1,
+      url: '/assets/gallery0.jpg',
+      comments: ['Hola', 'Bien']
+    },
+    {
+      id: 2,
+      url: '/assets/gallery1.jpg',
+      comments: ['Hola', 'Bien']
+    },
+    {
+      id: 3,
+      url: '/assets/gallery2.webp',
+      comments: []
+    },
+    {
+      id: 4,
+      url: '/assets/gallery3.jpeg',
+      comments: []
+    },
+    {
+      id: 5,
+      url: '/assets/gallery4.jpg',
+      comments: []
+    },
+    {
+      id: 6,
+      url: '/assets/gallery5.jpg',
+      comments: []
+    },
+    {
+      id: 7,
+      url: '/assets/gallery6.jpg',
+      comments: []
+    },
+    {
+      id: 8,
+      url: '/assets/gallery7.jpg',
+      comments: ['Hola', 'Bien']
+    },
+    {
+      id: 9,
+      url: '/assets/gallery8.webp',
+      comments: []
+    },
+    {
+      id: 10,
+      url: '/assets/gallery9.avif',
+      comments: []
+    }
+  ]);
+
+
 
 }
