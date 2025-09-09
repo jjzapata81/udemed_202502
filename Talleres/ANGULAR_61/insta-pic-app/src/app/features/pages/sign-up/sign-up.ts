f9ee47801e0365d4dd02b35e1e66ad45f1f1ae12
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2025-09-09 16:18:54 -0500

    taller 2 angular

--- Talleres/ANGULAR_61/insta-pic-app/src/app/features/pages/sign-up/sign-up.ts
+++ Talleres/ANGULAR_61/insta-pic-app/src/app/features/pages/sign-up/sign-up.ts
@@ -6,51 +6,51 @@ import { User } from "../../../shared/interfaces/user";
 import Swal from 'sweetalert2'
 
 @Component({
-    selector: 'app-sign-up',
-    imports: [RouterLink, ReactiveFormsModule],
-    templateUrl: './sign-up.html',
-    styleUrl: './sign-up.css'
+  selector: 'app-sign-up',
+  imports: [RouterLink, ReactiveFormsModule],
+  templateUrl: './sign-up.html',
+  styleUrl: './sign-up.css'
 })
 export class SignUp {
 
-    title = 'Registro de usuario';
+  title = 'Registro de usuario';
 
-    fb = inject(FormBuilder);
+  fb = inject(FormBuilder);
 
-    router = inject(Router);
+  router = inject(Router);
 
-    authService = inject(Auth);
+  authService = inject(Auth);
 
-    signUpForm = this.fb.group({
-        username: ['', [Validators.required]],
-        email: ['', [Validators.required]],
-        password: ['', [Validators.required, Validators.minLength(4)]],
-        rePassword: ['', [Validators.required, Validators.minLength(4)]]
-    })
+  signUpForm = this.fb.group({
+    username: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(4)]],
+    rePassword: ['', [Validators.required, Validators.minLength(4)]]
+  })
 
 
-    onSignUp() {
-        if (!this.signUpForm.valid) {
-            Swal.fire({
-                title: "Ops!",
-                text: "El formulario no es valido",
-                icon: "error"
-            });
-            return;
-        }
-        let user = this.signUpForm.value as User;
-
-        let response = this.authService.onSignUp(user);
-
-        if (!response.success) {
-            Swal.fire({
-                title: "Ops!",
-                text: response.message,
-                icon: "error"
-            });
-            return;
-        }
-        this.router.navigate([response.redirectTo]);
+  onSignUp() {
+    if (!this.signUpForm.valid) {
+      Swal.fire({
+        title: "Ops!",
+        text: "El formulario no es valido",
+        icon: "error"
+      });
+      return;
     }
+    let user = this.signUpForm.value as User;
+
+    let response = this.authService.onSignUp(user);
+
+    if (!response.success) {
+      Swal.fire({
+        title: "Ops!",
+        text: response.message,
+        icon: "error"
+      });
+      return;
+    }
+    this.router.navigate([response.redirectTo]);
+  }
 
 }
