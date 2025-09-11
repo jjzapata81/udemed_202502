29f6a85db61924d128290f0d80eb3c3c9e2a139a
Melissa <melissaforonda06@gmail.com>
2025-09-10 20:01:24 -0500

    Trabajo en clase

--- Talleres/ANGULAR/insta-pic-app/src/app/features/pages/upload/upload.ts
+++ Talleres/ANGULAR/insta-pic-app/src/app/features/pages/upload/upload.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Storage } from '../../../shared/services/storage';
+import { Auth } from '../../../shared/services/auth';
 
 @Component({
   selector: 'app-upload',
@@ -7,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './upload.css'
 })
 export class Upload {
+
+  storageService = inject(Storage);
+  authService = inject(Auth);
+
+  onUploadImage(event: Event){
+    let inputFile = event.target as HTMLInputElement;
+    if(!inputFile.files || inputFile.files.length <= 0){
+      return;
+    }
+
+    const imageFile = inputFile.files[0];
+    const username = this.authService.getUserLogged().username;
+    this.storageService.uploadFile(imageFile, username)
+    console.log(event);
+
+  }
 
 }
---
+++ Talleres/ANGULAR/insta-pic-app/src/app/shared/services/storage.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_KEY } from '../../../environments/environment';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Storage {
+
+  supabaseUrl = 'https://yukdnrhofokvxefwwclf.supabase.co'
+  supabase = createClient(this.supabaseUrl, SUPABASE_KEY)
+
+  async uploadFile(imageFile:File, username:string){
+    //const imageFile = event.target.files[0]
+    const fileName = uuidv4();
+    const { data, error } = await this.supabase.storage
+      .from('instapic')
+      .upload(`${username}/${fileName}`, imageFile);
+
+      console.log(data);
+      console.log(error);
+  }
+}
