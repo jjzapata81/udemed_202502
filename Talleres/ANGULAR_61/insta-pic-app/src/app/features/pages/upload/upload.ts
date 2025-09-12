b9a21371a7d7e4eb2c69f704b5b41fad9383a41d
s307e22 <profesor1@udem.edu.co>
2025-09-11 20:05:18 -0500

    supabase

--- Talleres/ANGULAR_61/insta-pic-app/src/app/features/pages/upload/upload.ts
+++ Talleres/ANGULAR_61/insta-pic-app/src/app/features/pages/upload/upload.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Storage } from '../../../shared/services/storage';
+import { Auth } from '../../../shared/services/auth';
 
 @Component({
   selector: 'app-upload',
@@ -7,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './upload.css'
 })
 export class Upload {
+
+  storageService = inject(Storage);
+  authService = inject(Auth);
+
+  onUploadFile(event:Event){
+    console.log(event);
+    const inputTarget = event.target as HTMLInputElement;
+
+    if(!inputTarget.files || inputTarget.files.length <=0){
+      return;
+    }
+    const imageFile = inputTarget.files[0];
+    const username = this.authService.getUserLogged().username;
+    this.storageService.uploadFile(imageFile,username);
+  }
 
 }
