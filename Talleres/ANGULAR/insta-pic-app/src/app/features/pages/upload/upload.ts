10ea3c021ad63a920856de6b45f5d6ad7d6863d4
thunderbol <jdvid.bermudez@gmail.com>
2025-09-15 20:29:21 -0500

    supabase 2

--- Talleres/ANGULAR/insta-pic-app/src/app/features/pages/upload/upload.ts
+++ Talleres/ANGULAR/insta-pic-app/src/app/features/pages/upload/upload.ts
@@ -1,6 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { Storage } from '../../../shared/services/storage';
 import { Auth } from '../../../shared/services/auth';
+import Swal from 'sweetalert2'
+import { Router } from '@angular/router';
+import { UserService } from '../../../shared/services/user-service';
 
 @Component({
   selector: 'app-upload',
@@ -12,19 +15,31 @@ export class Upload {
 
   storageService = inject(Storage);
   authService = inject(Auth);
+  userService = inject(UserService);
+  router = inject(Router);
 
 
-  onUploadImage(event:Event){
+  onUploadImage(event: Event) {
 
     let inputFile = event.target as HTMLInputElement;
-    if(!inputFile.files || inputFile.files.length <= 0){
+    if (!inputFile.files || inputFile.files.length <= 0) {
       return;
     }
 
     const imageFile = inputFile.files[0];
     const username = this.authService.getUserLogged().username;
-    this.storageService.uploadFile(imageFile,username)
-    console.log(event);
+    this.storageService.uploadFile(imageFile, username)
+      .then(response => {
+        if (response && response.data) {
+          const url = this.storageService.getImageUrl(response.data.fullPath);
+          this.userService.saveImage(username, url);
+        } else if (response) {
+          Swal.fire('Error!!')
+        }
+      }
+      );
+    this.router.navigate(['home'])
+
 
   }
 
--- Talleres/ANGULAR/insta-pic-app/src/app/shared/services/storage.ts
+++ Talleres/ANGULAR/insta-pic-app/src/app/shared/services/storage.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { createClient } from '@supabase/supabase-sc'
+import { createClient } from '@supabase/supabase-js'
 import { SUPABASE_KEY, SUPABASE_URL } from '../../../environments/environment';
+import { v4 as uuidv4 } from 'uuid';
 
 
 
@@ -12,13 +13,22 @@ export class Storage {
   supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
   async uploadFile(imagefile:File, username:string) {
-    //const file = event.target,files[0];
     const fileName = uuidv4();
-    const { data, error } = await this.supabase.storage
+   // const { data, error } = this.supabase.storage
+   return this.supabase.storage
       .from('instapic')
-      .upload(`${username}/avatar1.png`, imagefile)
-    console.log(data);
-    console.log(error);
+      .upload(`${username}/${fileName}`, imagefile)
+      .then(response=>{
+        return response;
+      })
+      .catch(error=>console.error(error))
+  //console.log(`4 - Despues de llamar a supabase`)
+   // console.log(data);
+   // console.log(error);
+  }
+
+  getImageUrl(fullPaht:string){
+    return `${SUPABASE_URL}/storage/v1/object/public/${fullPaht}`;
   }
 
 
