0e55a31dfa874edbdb9b27993c39dd99722ab0ec
jorgebls <iefaz.jorgebedoya@gmail.com>
2025-09-16 20:01:18 -0500

    feat: subida de cambios

---
+++ Talleres/ANGULAR_61/insta-pic-app/src/app/shared/services/user-service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  saveImage(username:string, imgeUrl:string){
+
+    const galleryItem = {
+      id: uuidv4(),
+      url: imgeUrl,
+      comments:[]
+    }
+
+    let galleryStr = localStorage.getItem(`${username}_gallery`);
+
+    if(galleryStr){
+      let galleryItems = JSON.parse(galleryStr);
+      galleryItems.push(galleryItem)
+      localStorage.setItem(`${username}_gallery`, JSON.stringify(galleryItems));
+      return;
+    }
+
+    const galleryItems = [galleryItem];
+    localStorage.setItem(`${username}_gallery`, JSON.stringify(galleryItems));
+  }
+
+  getGallery(username:string){
+    let galleryStr = localStorage.getItem(`${username}_gallery`);
+    if(galleryStr){
+      return JSON.parse(galleryStr);
+    }
+    return [];
+  }
+}
