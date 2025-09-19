65bde9bdc029c1ca8c74f7bcecc3657ff7ba20ce
s307e22 <profesor1@udem.edu.co>
2025-09-18 19:54:43 -0500

    creating api project

---
+++ Talleres/ANGULAR_61/insta-pic-api/src/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  
+  getHello(): string {
+    return 'Hola World!';
+  }
+
+  createHello(): string{
+    return 'Crear Hola';
+  }
+
+
+}
