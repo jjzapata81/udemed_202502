deaab6a14290e86e81e9dba56abca85e5af02627
Your Name <you@example.com>
2025-09-24 20:26:20 -0500

    Users CRUD

---
+++ Talleres/ANGULAR/insta-pic-api/src/user/entities/user.entity.ts
@@ -0,0 +1,11 @@
+export class User {
+    id:string;
+    username:string;
+    password:string;
+    name:string;
+    email:string;
+    avatarUrl?:string;
+    createdAt: Date;
+    updatedAt: Date;
+    isActive: boolean;
+}
