02b3a919d004777544322c986e547bb56e40dd0d
Your Name <you@example.com>
2025-09-30 19:53:44 -0500

    crud bd

--- Talleres/ANGULAR_61/insta-pic-api/src/app.module.ts
+++ Talleres/ANGULAR_61/insta-pic-api/src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    AuthModule, 
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'aws-1-us-east-2.pooler.supabase.com',
+      port: 5432,
+      username: 'postgres.jlkkpfvhavizoybkccez',
+      password: 'XXXXXXXXXXXXXX',
+      database: 'postgres',
+      entities: [User],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- Talleres/ANGULAR_61/insta-pic-api/src/user/entities/user.entity.ts
+++ Talleres/ANGULAR_61/insta-pic-api/src/user/entities/user.entity.ts
@@ -1,13 +1,33 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('users')
 export class User {
 
+    @PrimaryGeneratedColumn('uuid')
     id:string;
+
+    @Column({type:'varchar', unique:true})
     username:string;
+
+    @Column({type:'varchar'})
     password:string;
+    
+    @Column({type:'varchar', nullable:true})
     email?:string;
+    
+    @Column({type:'varchar', nullable:true})
     name?:string;
+    
+    @Column({type:'varchar', name:'avatar_url', nullable:true})
     avatarUrl?:string;
+
+    @CreateDateColumn({name:'created_at'})
     createdAt: Date;
+
+    @UpdateDateColumn({name:'updated_at'})
     updatedAt:Date;
+
+    @Column({type:'boolean', name:'is_active', default:true})
     isActive:boolean;
 
 }
