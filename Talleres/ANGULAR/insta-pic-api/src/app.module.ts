2f2592002256bf888ed106f963a4203faca95b68
Your Name <you@example.com>
2025-10-01 20:11:31 -0500

    relationship

--- Talleres/ANGULAR/insta-pic-api/src/app.module.ts
+++ Talleres/ANGULAR/insta-pic-api/src/app.module.ts
@@ -4,24 +4,25 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
+import { ImageModule } from './image/image.module';
 
 @Module({
   imports: [
     AuthModule, 
     UserModule,
+    ImageModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'aws-1-us-east-2.pooler.supabase.com',
-      port: 5432,
-      username: 'postgres.aobwxdnbjmttbynbnguv',
-      password: 'XXXXXXXXXXXXXXXXXXXX',
-      database: 'postgres',
-      entities: [User],
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       autoLoadEntities:true,
       synchronize: true,
     }),
-
   ],
   controllers: [AppController],
   providers: [AppService],
--- Talleres/ANGULAR/insta-pic-api/src/app.service.ts
+++ Talleres/ANGULAR/insta-pic-api/src/app.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
 
-  getHello(): string {
-    return 'Hola mundo!!!!!!!!';
+  getPing(): string {
+    return 'Ping...';
   }
 
 }
