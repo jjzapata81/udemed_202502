dc2429eed7c4b3e8a15580174dad57ed9d3f701b
s307e22 <profesor1@udem.edu.co>
2025-10-02 19:57:22 -0500

    relations

---
+++ Talleres/ANGULAR_61/insta-pic-api/src/photo/photo.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@nestjs/common';
+import { UploadPhotoDto } from './dto/upload-photo.dto';
+import { Repository } from 'typeorm';
+import { Photo } from './entities/photo.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AddCommentDto } from './dto/add-comment.dto';
+import { Comment } from './entities/comment.entity';
+
+@Injectable()
+export class PhotoService {
+    
+
+    constructor(
+        @InjectRepository(Photo)
+        private photoRepository: Repository<Photo>,
+        @InjectRepository(Comment)
+        private commentRepository: Repository<Comment>
+    ) {
+
+    }
+
+    uploadPhoto(uploadPhotoDto: UploadPhotoDto) {
+        const photoEntity = this.photoRepository.create(
+            {
+                url: uploadPhotoDto.url,
+                user: { id: uploadPhotoDto.userId }
+            }
+        );
+        return this.photoRepository.save(photoEntity);
+    }
+
+    addComment(addCommentDto: AddCommentDto) {
+        const commentEntity = this.commentRepository.create(
+            {
+                message: addCommentDto.message,
+                user: { id: addCommentDto.userId },
+                photo: { id: addCommentDto.photoId }
+            }
+        );
+        return this.commentRepository.save(commentEntity);
+    }
+
+    findByUserId(userId: string) {
+      /*return this.photoRepository.findBy(
+        {
+            user:{ id: userId, isActive:true}
+        }
+      );*/
+      return this.photoRepository.find(
+        {
+            where:{
+                user:{id:userId, isActive:true}
+            },
+            relations:{
+                comments:true,
+                user:true
+            },
+            select:{
+                id:true,
+                url:true,
+                createdAt:true,
+                user:{ username:true, id:true }
+            }
+        }
+      )
+    }
+
+}
