6960d1551079e0f1e0fb492bf1d6b385b5ee199e
David <david.lopez.rivera.2003@gmail.com>
2025-10-03 01:00:04 -0500

    taller_clase

---
+++ Talleres/ANGULAR_61/insta-pic-api/src/photo/dto/add-comment.dto.ts
@@ -0,0 +1,5 @@
+export class AddCommentDto{
+    message : string;
+    userId:string;
+    photoId: string;
+}
---
+++ Talleres/ANGULAR_61/insta-pic-api/src/photo/photo.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { PhotoService } from './photo.service';
+import { UploadPhotoDto } from './dto/upload-photo.dto';
+import { AddCommentDto } from './dto/add-comment.dto';
+
+@Controller('v1/gallery')
+export class PhotoController {
+  constructor(private readonly photoService: PhotoService) {}
+
+    @Post('add')
+    uploadPhoto(@Body() uploadPhotoDto: UploadPhotoDto){
+
+    return this.photoService.uploadPhoto(uploadPhotoDto);   
+   }
+
+   @Post('comment/add')
+   AddCommnet(@Body() addCommentDto: AddCommentDto){
+    return this.photoService.addComment(addCommentDto)
+   }
+
+   @Get(':userId')
+   getByUser(@Param('userId')  userId:string){
+    return this.photoService.findByUserId(userId)
+}
+
+}
