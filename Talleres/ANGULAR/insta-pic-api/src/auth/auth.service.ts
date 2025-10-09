7fb49fa2a8167b03ec53d354cabeadb10419bf6d
Your Name <you@example.com>
2025-10-08 19:57:40 -0500

    jwt

--- Talleres/ANGULAR/insta-pic-api/src/auth/auth.service.ts
+++ Talleres/ANGULAR/insta-pic-api/src/auth/auth.service.ts
@@ -3,11 +3,15 @@ import { LoginDto } from './dto/create-auth.dto';
 import bcrypt from "bcryptjs";
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
-  constructor(private userService: UserService){
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService
+  ){
   }
 
   users = [
@@ -20,17 +24,12 @@ export class AuthService {
     console.log(loginDto);
 
     const user = await this.userService.findByUsername(loginDto.username);
+    if(user &&  bcrypt.compareSync(loginDto.password, user.password)){ 
+      const payload = {username:user.username, id:user.id, avatarUrl:user.avatarUrl}
 
-    //const user = this.users.find(user=>user.username===loginDto.username);
-    
-
-
-    //if(user && user.password===loginDto.password){
-
-    if(user &&  bcrypt.compareSync(loginDto.password, user.password)){  
       return {
         success:true,
-        token:'EWASDYJGDLWKHFLNEGLENGLNGELKNGÑ'
+        token: await this.jwtService.signAsync(payload)
       }
     }
     throw new NotFoundException('Usuario o contraseña incorrectos');
