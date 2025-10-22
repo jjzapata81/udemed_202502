4faaecbc30abb1a69269702c8e9d24024aa4ac7d
Tatiana Murillo <tatianamurillomosquera1@gmail.com>
2025-10-21 19:57:29 -0500

    Termino del primer punto

--- Talleres/ANGULAR_61/insta-pic-api/src/auth/auth.module.ts
+++ Talleres/ANGULAR_61/insta-pic-api/src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot(),
     JwtModule.register({
       global: true,
-      secret: 'yghhfghgffghfjh',
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '2h' },
     }),
   ],
