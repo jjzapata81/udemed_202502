ba3382328a176f821f98989e406ca4d057459293
Sarapv09 <sara.pineda.v@outlook.com>
2025-10-22 16:28:07 -0500

    avance ultimo trabajo en clase

--- Talleres/ANGULAR/insta-pic-app/src/app/shared/services/auth.ts
+++ Talleres/ANGULAR/insta-pic-app/src/app/shared/services/auth.ts
@@ -2,7 +2,7 @@ import { inject, Injectable, signal } from '@angular/core';
 import { User } from '../interfaces/user';
 import { LoginRespose, LoginServiceResponse, SignUpResponse } from '../interfaces/login-response';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map, Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { JwtService } from './jwt-service';
 
 @Injectable({
@@ -11,7 +11,7 @@ import { JwtService } from './jwt-service';
 export class Auth {
 
   http = inject(HttpClient);
-  jwtService = inject(JwtService)
+  jwtService = inject(JwtService);
 
   isLoged = signal(false);
 
@@ -21,12 +21,12 @@ export class Auth {
 
   login(user: User): Observable<LoginRespose> {
     return this.http.post<LoginServiceResponse>('http://localhost:3000/api/v1/auth/login', user).pipe(
-      map(response => {
+      map((response) => {
         sessionStorage.setItem('token', response.token);
         this.verifyLoggedUser();
         return {
           success: response.success
-        }
+        };
       }),
       catchError((error) => {
         return [{ success: false, message: 'Usuario o contraseña incorrectos' }];
@@ -36,18 +36,16 @@ export class Auth {
   }
 
 
-  signUp(user: User): SignUpResponse {
-
-    //this.http.post('http://localhost:3000/api/v1/user', user)
-
-    let userStr = localStorage.getItem(user.username!);
-    if (userStr) {
-      return { success: false, message: 'Ya existe el Usuario' };
-    }
-    localStorage.setItem(user.username!, JSON.stringify(user));
-    sessionStorage.setItem('userLogged', user.username);
-    this.verifyLoggedUser();
-    return { success: true, redirectTo: 'home' };
+  signUp(user: User): Observable<SignUpResponse> {
+    return this.http.post<any>('http://localhost:3000/api/v1/user', user).pipe(
+      map(() => {
+        return { success: true, redirectTo: 'home' } as SignUpResponse;
+      }),
+      catchError((error) => {
+        console.error('Error al crear el usuario:', error);
+        return of({ success: false, message: 'Error al crear el usuario' } as SignUpResponse);
+      })
+    );
   }
 
   logout() {
