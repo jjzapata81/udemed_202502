0fcba2c6078febcb794d6b05055c44fbd69eb9b0
Felipe Villa <villajaramillofelipe4@gmail.com>
2025-10-22 19:21:36 -0500

    feat(upload): Implemented backend connection for upload a picture

--- Talleres/ANGULAR/insta-pic-app/src/app/features/pages/upload/upload.ts
+++ Talleres/ANGULAR/insta-pic-app/src/app/features/pages/upload/upload.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { Storage } from '../../../shared/services/storage';
 import { Auth } from '../../../shared/services/auth';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
 import { UserService } from '../../../shared/services/user-service';
 
@@ -9,10 +9,9 @@ import { UserService } from '../../../shared/services/user-service';
   selector: 'app-upload',
   imports: [],
   templateUrl: './upload.html',
-  styleUrl: './upload.css'
+  styleUrl: './upload.css',
 })
 export class Upload {
-
   storageService = inject(Storage);
   authService = inject(Auth);
   userService = inject(UserService);
@@ -33,20 +32,28 @@ export class Upload {
         Swal.showLoading();
       }
     });*/
-    this.storageService.uploadPicture(imageFile, user.username)
-      .then(fullPath => {
-        const imageUrl = this.storageService.getUrl(fullPath);
-        this.userService.saveImage(user.id!, imageUrl);
-      })
-      .catch(error => {
-        console.log(error);
-        Swal.fire({
-          text: 'Error al cargar la imagen',
-          icon: 'error'
-        })
+    this.storageService.uploadPicture(imageFile, user.username).then((fullPath) => {
+      const imageUrl = this.storageService.getUrl(fullPath);
+      console.log('🚀 ~ Upload ~ onUploadFile ~ id:', user.id);
+      console.log('🚀 ~ Upload ~ onUploadFile ~ imageUrl:', imageUrl);
+      this.userService.saveImage(user.id!, imageUrl).subscribe({
+        next: () => {
+          Swal.fire({
+            text: 'Imagen cargada exitosamente',
+            icon: 'success',
+          });
+          this.router.navigate(['home']);
+        },
+        error: (error) => {
+          console.error(error);
+          Swal.fire({
+            text: 'Ocurrió un error al subir la imagen',
+            icon: 'error',
+          });
+        },
       });
-    this.router.navigate(['home'])
+    });
+
     //Swal.close();
   }
-
 }
--- Talleres/ANGULAR/insta-pic-app/src/app/shared/services/user-service.ts
+++ Talleres/ANGULAR/insta-pic-app/src/app/shared/services/user-service.ts
@@ -1,48 +1,42 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { User } from '../interfaces/user';
-import { v4 as uuidv4 } from 'uuid';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { JwtService } from './jwt-service';
+import { catchError, map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  jwtService = inject(JwtService);
 
-  saveImage(userId:string, url:string){
+  http = inject(HttpClient);
 
-    const galleryItem = {
-      id: uuidv4(),
-      url:url,
-      comments:[]
-    }
-
-    let galleryStr = localStorage.getItem(`${userId}_gallery`);
-
-    if(galleryStr){
-      let galleryItems = JSON.parse(galleryStr);
-      galleryItems.push(galleryItem)
-      localStorage.setItem(`${userId}_gallery`, JSON.stringify(galleryItems));
-      return;
-    }
-
-    const galleryItems = [galleryItem];
-    localStorage.setItem(`${userId}_gallery`, JSON.stringify(galleryItems));
+  saveImage(userId: string, url: string) {
+    const headers = new HttpHeaders({ Authorization: `Bearer ${this.jwtService.getToken()}` });
+    return this.http
+      .post('http://localhost:3000/api/v1/gallery/add', { userId, url }, { headers })
+      .pipe(
+        map(() => {
+          return { success: true };
+        }),
+        catchError((error) => {
+          throw new Error(error.message);
+        })
+      );
   }
 
-  getGallery(userId:string){
+  getGallery(userId: string) {
     let galleryStr = localStorage.getItem(`${userId}_gallery`);
-    if(galleryStr){
+    if (galleryStr) {
       return JSON.parse(galleryStr);
     }
     return [];
-
   }
 
   findAll() {
     //throw new Error('Method not implemented.');
   }
 
-  update(userId:string, user:Partial<User>) {
-
-  }
-  
+  update(userId: string, user: Partial<User>) {}
 }
